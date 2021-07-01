84c3413592eb6511aabe47ab9822eb3a05ba6d23
peipan <peipan96@163.com>
2021-07-01 23:07:20 +0800

    init repo

---
+++ src/main/java/com/pei/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.pei.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by peipan on 2021/7/1
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
