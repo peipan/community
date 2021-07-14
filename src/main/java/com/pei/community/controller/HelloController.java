8050585c06478b75b2c89b943c3df0434a322e32
peipan <peipan96@163.com>
2021-07-14 22:37:05 +0800

    login states persistence

--- src/main/java/com/pei/community/controller/HelloController.java
+++ src/main/java/com/pei/community/controller/HelloController.java
@@ -1,9 +1,17 @@
 package com.pei.community.controller;
 
+import com.pei.community.mapper.UserMapper;
+import com.pei.community.model.User;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by peipan on 2021/7/1
@@ -11,8 +19,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/com/pei/community/mapper/UserMapper.java
+++ src/main/java/com/pei/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.pei.community.mapper;
 import com.pei.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,4 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
     @Insert("insert into user (name, account_id, token, gtm_create, gtm_modified) values (#{name}, #{accountId}, #{token}, #{gtmCreate}, #{gtmModified})")
     public void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+
 }
